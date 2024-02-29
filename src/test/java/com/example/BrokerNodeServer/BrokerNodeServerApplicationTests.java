95165ebad01fa635bdede93154c0c3d86277db19
Radhika Joshi <radhika7002@gmail.com>
2024-02-29 13:26:54 -0800

    first commit

---
+++ src/test/java/com/example/BrokerNodeServer/BrokerNodeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BrokerNodeServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerNodeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
